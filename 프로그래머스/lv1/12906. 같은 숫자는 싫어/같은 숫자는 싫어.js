e7fd7a14e740a662ed06b989d610d2bd63d630dd
샘 <68241138+saemileee@users.noreply.github.com>
2023-06-28 00:13:00 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 43.73 ms, Memory: 83 MB -BaekjoonHub

--- 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -1,11 +1,9 @@
 function solution(arr)
 {
     const answer = [arr[0]];
-    let prev = arr[0];
     for(let i = 1;i<arr.length;i++){
-        if(prev!==arr[i]){
+        if(answer[answer.length-1]!==arr[i]){
             answer.push(arr[i]);
-            prev=arr[i];
         };
     };
     return answer;

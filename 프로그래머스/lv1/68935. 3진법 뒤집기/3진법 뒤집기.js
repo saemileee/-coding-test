e94ed8a669dce7da71f515000343183eb5e2e5a9
샘 <68241138+saemileee@users.noreply.github.com>
2023-06-29 23:08:33 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.js
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.js
@@ -1,13 +1,3 @@
 function solution(n) {
-    let answer = 0;
-    let remainers = [];
-    while(n>0){
-        remainers.push(n%3);
-        n = Math.floor(n/3);        
-    }
-    
-    for(let i = 0;i<remainers.length;i++){
-        answer += Math.pow(3, remainers.length-1-i)*remainers[i];
-    }
-    return answer;
+    return parseInt([...n.toString(3)].reverse().join(''),3);
 }

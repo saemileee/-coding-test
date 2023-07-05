5b3a35a9005865582a90fe4246763d8acea67331
샘 <68241138+saemileee@users.noreply.github.com>
2023-07-05 17:38:31 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.27 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.js
+++ 프로그래머스/lv1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.js
@@ -1,23 +1,13 @@
 function solution(dartResult) {
     const scores = [];
-    const regex = /\d([\d]?)+[A-Z]{1}([*#]?)/g;
-    const scoreRegex = /\d{1,2}|[A-Z]{1}|[*#]/g;
+    const bonusMap = {S:1,D:2,T:3};
+    const regex = /\d([\d]?)+[SDT]{1}([*#]?)/g;
+    const scoreRegex = /\d{1,2}|[SDT]{1}|[*#]/g;
     const dartResultArr = dartResult.match(regex).map((result)=>result.match(scoreRegex));
     for (let i=0;i<dartResultArr.length;i++){
         dartResultArr[i][0] *= 1; 
         const [score, bonus, option] = dartResultArr[i];
-        let amount = 0;
-        switch (bonus){
-            case 'S':
-                amount = score;
-                break;
-            case 'D':
-                amount = score**2;
-                break;
-            case 'T':
-                amount = score**3;
-                break;
-        }
+        let amount = Math.pow(score,bonusMap[bonus])
         if (option==="*"){
             if(i===0){
                 amount *= 2; 

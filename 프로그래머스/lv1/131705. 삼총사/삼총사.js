c07331f77a6ef0e11c0d4cd5a993f482bf9e2b52
샘 <68241138+saemileee@users.noreply.github.com>
2023-08-15 18:19:53 +0900

    [level 1] Title: 삼총사, Time: 3.29 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.js
@@ -0,0 +1,15 @@
+function solution(number) {
+    var answer = 0;
+    for(let i = 0; i<number.length; i++){
+        for(let j = i+1; j<number.length; j++){
+            for(let k = j+1; k<number.length; k++){
+                const sum = number[i] + number[j] + number[k];
+                if(sum===0){
+                    console.log(number[i], number[j], number[k]);
+                    answer ++;
+                }
+            }
+        }
+    }
+    return answer;
+}

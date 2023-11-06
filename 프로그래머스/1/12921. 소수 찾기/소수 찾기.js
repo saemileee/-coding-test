e764aec69a0a4e31a4df58473c03944159cfe533
샘 <68241138+saemileee@users.noreply.github.com>
2023-11-06 13:38:38 +0900

    [level 1] Title: 소수 찾기, Time: 161.13 ms, Memory: 61.4 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.js
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.js
@@ -1,16 +1,17 @@
 function solution(n) {
-    let answer = 0;
-    const arr = Array.from({length:n}, (v,i) => i!==1?i:0);
-    for (let i = 2; i<=n; i++){
-        if(arr[i]===0) continue;
-        answer += 1;
-        arr[i] = 0;
-        
-        for(let j = i*2; j <= n; j += i){
-            if(arr[j] === 0) continue;
-            arr[j] = 0;
-        }
-        
+    const s = new Set();
+    for(let i = 1; i<=n ; i+=2){
+        s.add(i);
     }
-    return answer;
+    s.delete(1);
+    s.add(2);
+    
+    for(let i=3; i<Math.sqrt(n); i++){
+        if(s.has(i)){
+            for(let j = i*2; j<=n; j+=i){
+                s.delete(j);
+            }
+        }
+    }
+    return s.size;
 }
